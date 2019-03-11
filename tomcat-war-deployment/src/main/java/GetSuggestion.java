a43cd7f089197a7cbc66251957ff5b9a9ccaabb7
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-03-10 20:15:40 -0500

    Add files via upload

--- tomcat-war-deployment/src/main/java/GetSuggestion.java
+++ tomcat-war-deployment/src/main/java/GetSuggestion.java
@@ -3,25 +3,53 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.common.TasteException;
+import java.util.List;
+
 // Extend HttpServlet class
 public class GetSuggestion extends HttpServlet {
 
-   private String message;
-
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World";
    }
 
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-      // Set response content type
       response.setContentType("text/html");
-
-      // Actual logic goes here.
       PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
+
+      ServletContext context = request.getServletContext();
+      String fullPath = context.getRealPath("/WEB-INF/classes/dataset.csv");
+
+      try {
+        DataModel model = new FileDataModel(new File(fullPath));
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+        UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+        UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+        List<RecommendedItem> recommendations = recommender.recommend(2, 3);
+        for (RecommendedItem recommendation : recommendations) {
+          out.println(recommendation);
+        }
+      } catch(TasteException ex2) {
+        ex2.printStackTrace();
+      }
+
+   }
+
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
    }
 
    public void destroy() {
