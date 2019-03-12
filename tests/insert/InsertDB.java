6ea1ed2079cfc012f07343ba99145e6d57b425de
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-03-12 02:38:46 -0500

    Create InsertDB.java

---
+++ tests/insert/InsertDB.java
@@ -0,0 +1,37 @@
+
+import java.sql.*;
+import java.util.Random;
+
+public class InsertDB {
+
+  public static void main (String[] args){
+
+    Random rand = new Random();
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection con = DriverManager.getConnection(
+      "jdbc:mysql://localhost:3306/cs4333",
+      "dbuser",
+      "mariadb"
+      );
+
+      Statement st = con.createStatement();
+      int userID;
+      int itemID;
+      int rating;
+
+      for(int i = 0; i < 500; i++) {
+        userID = rand.nextInt(150)+100;
+        itemID = rand.nextInt(50)+100;
+        rating = rand.nextInt(5);
+        st.executeUpdate("INSERT INTO history(user_id,item_id,rating) VALUES ("+userID+","+itemID+","+rating+")");
+      }
+
+      con.close();
+
+    } catch(Exception ex) {
+      System.out.println(ex.getMessage());
+    }
+  }
+
+}
