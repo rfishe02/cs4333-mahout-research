163bf00ca7ef03eb84c92963007a932a95681152
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-03-12 21:42:00 -0500

    Add files via upload

--- tests/insert/InsertDB.java
+++ tests/insert/InsertDB.java
@@ -1,6 +1,7 @@
 
 import java.sql.*;
 import java.util.Random;
+import java.io.*;
 
 public class InsertDB {
 
@@ -15,18 +16,21 @@ public class InsertDB {
       "mariadb"
       );
 
+      PrintWriter pw = new PrintWriter(new FileWriter("dataset.csv"));
       Statement st = con.createStatement();
       int userID;
       int itemID;
       int rating;
 
       for(int i = 0; i < 500; i++) {
-        userID = rand.nextInt(100)+1;
+        userID = rand.nextInt(15)+1;
         itemID = rand.nextInt(30)+1;
-        rating = rand.nextInt(5);
+        rating = rand.nextInt(5)+1;
         st.executeUpdate("INSERT INTO history(user_id,item_id,rating) VALUES ("+userID+","+itemID+","+rating+")");
+        pw.write(userID+","+itemID+","+rating+"\n");
       }
 
+      pw.close();
       con.close();
 
     } catch(Exception ex) {
