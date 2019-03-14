91dc67f29426e8e2608261cedec89f22c82d35e5
rfishe02 <43683461+rfishe02@users.noreply.github.com>
2019-03-14 01:15:33 -0500

    Add files via upload

--- tests/insert/InsertDB.java
+++ tests/insert/InsertDB.java
@@ -2,11 +2,13 @@
 import java.sql.*;
 import java.util.Random;
 import java.io.*;
+import java.util.HashSet;
 
 public class InsertDB {
 
   public static void main (String[] args){
 
+    HashSet<String> set = new HashSet<>();
     Random rand = new Random();
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
@@ -18,16 +20,25 @@ public class InsertDB {
 
       PrintWriter pw = new PrintWriter(new FileWriter("dataset.csv"));
       Statement st = con.createStatement();
-      int userID;
       int itemID;
       int rating;
 
-      for(int i = 0; i < 300; i++) {
-        userID = rand.nextInt(6)+1;
-        itemID = rand.nextInt(30)+1;
-        rating = rand.nextInt(5)+1;
-        st.executeUpdate("INSERT INTO history(user_id,item_id,rating) VALUES ("+userID+","+itemID+","+rating+")");
-        pw.write(userID+","+itemID+","+rating+"\n");
+      for(int i = 1; i <= 16; i++) {
+
+        for(int j = 0; j < 12; j++) {
+
+          itemID = rand.nextInt(16)+1;
+          rating = rand.nextInt(5)+1;
+
+          if(!set.contains(i+","+itemID)) {
+            st.executeUpdate("INSERT INTO history(user_id,item_id,rating) VALUES ("+i+","+itemID+","+rating+")");
+            pw.write(i+","+itemID+","+rating+"\n");
+
+            set.add(i+","+itemID);
+          }
+
+        }
+
       }
 
       pw.close();
